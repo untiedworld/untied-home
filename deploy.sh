a93d927b854d5fe7b48eabfbaa19eb09f7f39057
Erik Forsberg <erik.c.forsberg@gmail.com>
2021-12-14 21:50:43 +0100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# clean dist folder
+rm -rf dist
+
+# build
+yarn build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+echo 'dev.gokonnect.io' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:ankhuve/ankhuve.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f --dry-run git@github.com:ankhuve/konnect-web.git master:gh-pages
+
+cd -
